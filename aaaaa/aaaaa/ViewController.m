4f8e57a0ec0faff64859545e0d40eeedc5838dea
tarena237 <tarena237@tarena237deiMac.local>
2015-07-01 11:27:36 +0800

    错误

---
+++ aaaaa/aaaaa/ViewController.m
@@ -0,0 +1,26 @@
+//
+//  ViewController.m
+//  aaaaa
+//
+//  Created by tarena237 on 15/7/1.
+//  Copyright (c) 2015年 tarena. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"....");}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
